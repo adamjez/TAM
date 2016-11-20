d15e05833bdec047d8877ef27468bcfba32957ac
Ivan Sevcik <BetaRavener@users.noreply.github.com>
2016-11-20 15:28:33 +0100

    About page.

--- OnRadio.App/Views/About.xaml.cs
+++ OnRadio.App/Views/About.xaml.cs
@@ -1,4 +1,8 @@
-﻿using OnRadio.App.Common;
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.System;
+using Windows.UI.Xaml;
+using OnRadio.App.Common;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,6 +16,35 @@ namespace OnRadio.App.Views
         public About()
         {
             this.InitializeComponent();
+
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            AppName.Text = $"{packageId.Name}";
+            Version.Text = $"Verze {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        private async void OpenPlayCzWeb(object sender, RoutedEventArgs e)
+        {
+            const string playCzWebUrl = @"http://www.play.cz/";
+            var uri = new Uri(playCzWebUrl);
+
+            await Launcher.LaunchUriAsync(uri);
+        }
+
+        private async void OpenRateApp(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri(string.Format("ms-windows-store:REVIEW?PFN={0}", Package.Current.Id.FamilyName));
+            await Launcher.LaunchUriAsync(uri);
+        }
+
+        private async void SendFeedback(object sender, RoutedEventArgs e)
+        {
+            const string emailAddress = "mailto:?to=ivan-sevcik@hotmail.com&subject=Nazor na aplikaci";
+            var uri = new Uri(emailAddress);
+
+            await Launcher.LaunchUriAsync(uri);
         }
     }
 }
